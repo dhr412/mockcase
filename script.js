307633c8db44dbfcab2eb1514a9fd318728a2c42
Dhruv <62135445+dhr412@users.noreply.github.com>
2026-03-13 20:12:42 +0530

    Add copy button

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ function mockcase(str) {
 const output = document.querySelector("#out");
 const button = document.querySelector("#submitBtn");
 const input = document.querySelector("#textInp");
+const copyBtn = document.querySelector("#copyBtn");
+
 document.addEventListener("DOMContentLoaded", () => {
     input.addEventListener("keyup",(event) => {
         if (event.key === "Enter") {
@@ -45,4 +47,14 @@ button.addEventListener("click", (event) => {
     event.preventDefault();
     const text = input.value.toLowerCase();
     output.innerHTML = mockcase(text);
+})
+copyBtn.addEventListener("click", () => {
+    if (output.textContent) {
+        navigator.clipboard.writeText(output.textContent);
+        const originalText = copyBtn.innerText;
+        copyBtn.innerText = "Copied!";
+        setTimeout(() => {
+            copyBtn.innerText = originalText;
+        }, 2000);
+    }
 })
